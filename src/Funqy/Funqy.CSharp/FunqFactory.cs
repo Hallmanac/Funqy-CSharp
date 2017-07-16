c87f7fd02843eb2c5a1957285faae653d84f38c7
Brian Hall <brian_academyjunction@outlook.com>
2017-07-16 14:05:01 -0400

    Created .net standard version of this library

---
+++ src/Funqy/Funqy.CSharp/FunqFactory.cs
@@ -0,0 +1,71 @@
+﻿namespace Funqy.CSharp
+{
+    /// <summary>
+    /// A factory class to help generate new FunqResult objects
+    /// </summary>
+    public class FunqFactory
+    {
+        /// <summary>
+        /// Creates a new <see cref="FunqResult"/> with the IsSuccessful property set to true.
+        /// </summary>
+        /// <param name="message">Optional message that is saved to the Message property on the <see cref="FunqResult"/></param>
+        public static FunqResult Ok(string message = null)
+        {
+            return new FunqResult(true, message);
+        }
+
+
+        /// <summary>
+        /// Creates a new <see cref="FunqResult{T}"/> with the IsSuccessful property set to true.
+        /// </summary>
+        /// <param name="result">The underlying value of the new <see cref="FunqResult{T}"/></param>
+        /// <param name="message">Optional message that is saved to the Message property on the <see cref="FunqResult{T}"/></param>
+        public static FunqResult<T> Ok<T>(T result, string message = null)
+        {
+            return new FunqResult<T>(result, true, message);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="FunqResult{T}"/> with the IsSuccessful property set to true.
+        /// </summary>
+        /// <typeparam name="T">The type of the underlying Value property</typeparam>
+        /// <param name="funqResultResult">An existing FunqResult to pass along</param>
+        /// <param name="message">Optional message that is saved to the Message property on the <see cref="FunqResult{T}"/></param>
+        public static FunqResult<T> Ok<T>(FunqResult<T> funqResultResult, string message = null)
+        {
+            return new FunqResult<T>(funqResultResult.Value, true, message);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="FunqResult"/> with the IsSuccessful property set to false.
+        /// </summary>
+        /// <param name="message">Optional message that is saved to the Message property on the <see cref="FunqResult"/></param>
+        public static FunqResult Fail(string message)
+        {
+            return new FunqResult(false, message);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="FunqResult{T}"/> with the IsSuccessful property set to false.
+        /// </summary>
+        /// <typeparam name="T">The type of the underlying Value value</typeparam>
+        /// <param name="message">Optional message that is saved to the Message property on the <see cref="FunqResult{T}"/></param>
+        /// <param name="result">The underlying value of the new <see cref="FunqResult{T}"/></param>
+        public static FunqResult<T> Fail<T>(string message, T result)
+        {
+            return new FunqResult<T>(result, false, message);
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="FunqResult{T}"/> with the IsSuccessful property set to false.
+        /// </summary>
+        /// <typeparam name="T">The type of the underlying Value value</typeparam>
+        /// <param name="message">Optional message that is saved to the Message property on the <see cref="FunqResult{T}"/></param>
+        /// <param name="funqResultResult">An existing FunqResult to pass along</param>
+        /// <returns></returns>
+        public static FunqResult<T> Fail<T>(string message, FunqResult<T> funqResultResult)
+        {
+            return funqResultResult == null ? new FunqResult<T>(default(T), false, message) : new FunqResult<T>(funqResultResult.Value, false, message);
+        }
+    }
+}
---
+++ src/Funqy/Funqy.CSharp/FunqResult.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Funqy.CSharp
+{
+    /// <summary>
+    /// Class to help write C# in a more functional style. Coupled with extension methods from ExtendYourFunq.cs.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class FunqResult<T> : FunqResult
+    {
+        public FunqResult(T value, bool isSuccessful, string message = null)
+            : base(isSuccessful, message)
+        {
+            Value = value;
+        }
+
+
+        public T Value { get; }
+    }
+
+
+    /// <summary>
+    /// Class to help write C# in a more functional style. Coupled with extension methods from ExtendYourFunq.cs.
+    /// </summary>
+    public class FunqResult
+    {
+        public FunqResult(bool isSuccessful, string message)
+        {
+            if (!isSuccessful && string.IsNullOrWhiteSpace(message))
+            {
+                throw new InvalidOperationException("No error message provided for a non-successful value");
+            }
+            IsSuccessful = isSuccessful;
+            Message = message;
+        }
+
+
+        public bool IsSuccessful { get; }
+
+        public string Message { get; }
+
+        public bool IsFailure => !IsSuccessful;
+    }
+}
