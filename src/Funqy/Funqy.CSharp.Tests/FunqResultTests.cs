afe86ba082f664c15ee4ab79042b08b8fcb37c91
Brian Hall <brian_academyjunction@outlook.com>
2017-07-16 15:06:15 -0400

    Added single unit test for now

---
+++ src/Funqy/Funqy.CSharp.Tests/FunqResultTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Funqy.CSharp.Tests
+{
+    [TestClass]
+    public class FunqResultTests
+    {
+        [TestMethod]
+        public void FunqResult_Should_Fail_If_Not_Successful_And_Message_Is_Null_Or_WhiteSpace()
+        {
+            try
+            {
+                var funqResult = FunqFactory.Fail(" ");
+                Assert.Fail("InvalidOperationException was not thrown as expected.");
+            }
+            catch (InvalidOperationException e)
+            {
+                Assert.AreEqual("No error message provided for a non-successful value", e.Message, "The expected messages didn't match up. Please correct the \"expected\" value");
+            }
+        }
+    }
+}
