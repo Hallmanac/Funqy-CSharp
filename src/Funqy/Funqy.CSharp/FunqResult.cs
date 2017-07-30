9301d781afc5f4cc98c3e25053b24097d7e476e3
Brian Hall <brian_academyjunction@outlook.com>
2017-07-30 10:08:42 -0400

    Added some missing XML documentation comments.

--- src/Funqy/Funqy.CSharp/FunqResult.cs
+++ src/Funqy/Funqy.CSharp/FunqResult.cs
@@ -3,28 +3,56 @@
 namespace Funqy.CSharp
 {
     /// <summary>
-    /// Class to help write C# in a more functional style. Coupled with extension methods from ExtendYourFunq.cs.
+    /// Class to hold the result of a function that wraps an operation which will return a value.
     /// </summary>
     /// <typeparam name="T"></typeparam>
     public class FunqResult<T> : FunqResult
     {
+        /// <summary>
+        /// Constructor for the FunqResult with a Value result
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="isSuccessful"></param>
+        /// <param name="message"></param>
         public FunqResult(T value, bool isSuccessful, string message = null)
             : base(isSuccessful, message)
         {
             Value = value;
         }
 
-        public bool HasValue => Value != null;
 
+        /// <summary>
+        /// Returns whether the <see cref="Value"/> property contains a value and that it's NOT Empty or Whitespace in the case of a string
+        /// </summary>
+        public bool HasValue
+        {
+            get
+            {
+                if (typeof(T) != typeof(string))
+                    return Value != null;
+                var strVal = Value as string;
+                return !string.IsNullOrWhiteSpace(strVal);
+            }
+        }
+
+        /// <summary>
+        /// The Value of the result. This could potentially be nullable. It is recommended that you check
+        /// the <see cref="HasValue"/> property before trying to access this value.
+        /// </summary>
         public T Value { get; }
     }
 
 
     /// <summary>
-    /// Class to help write C# in a more functional style. Coupled with extension methods from ExtendYourFunq.cs.
+    /// Class to hold the result of a function that wraps an action operation which will NOT return a value.
     /// </summary>
     public class FunqResult
     {
+        /// <summary>
+        /// Constructor for a FunqResult without a value
+        /// </summary>
+        /// <param name="isSuccessful"></param>
+        /// <param name="message"></param>
         public FunqResult(bool isSuccessful, string message)
         {
             if (!isSuccessful && string.IsNullOrWhiteSpace(message))
@@ -36,10 +64,19 @@ namespace Funqy.CSharp
         }
 
 
+        /// <summary>
+        /// Determines whether or not the operation was successful
+        /// </summary>
         public bool IsSuccessful { get; }
 
+        /// <summary>
+        /// Text that can be used to convey an error or success message
+        /// </summary>
         public string Message { get; }
 
+        /// <summary>
+        /// Determines whether the operation was a failure
+        /// </summary>
         public bool IsFailure => !IsSuccessful;
     }
 }
